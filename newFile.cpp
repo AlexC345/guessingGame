a8c98884c891c9230bec2908c3a5b902e60f2036
U-BSD\461711 <461711@SHS-GYBF6X3.beaverton.k12.or.us>
2025-09-02 10:10:31 -0700

    firstCommit

---
+++ newFile.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+
+int main() {
+	//makes the playGame variable loop
+	char playGame = 'y';
+	while (playGame == 'y'){
+		//makes a random chosen number from 0 to 100
+		srand(static_cast<unsigned>(time(0)));
+		int chosenNumber = rand() % 101;
+		//makes a # of guesses variable
+		int guesses = 1;
+		cout << "Guessing game." << "\n"; //the title of the game
+		int number; //gets player input of first guess
+		cout << "Please enter a guessed number: ";
+		cin >> number;
+
+		while (number != chosenNumber){
+			//compares guesses number to chosen number
+			if (number > chosenNumber){
+				cout << "Too high!" << "\n";
+			}
+			else if (number < chosenNumber){
+				cout << "Too low!" << "\n";
+			}
+			//asks player for new guessed number and increase guesses by 1
+			cout << "Please enter a guessed number: ";
+			cin >> number;	
+			guesses += 1;
+			//if you guess the correct number, it breaks out of the while loop
+		}
+		//prints when player won and prints # of guesses
+		cout << "Congratulations! You guessed the number!" << "\n";
+		cout << "Guesses: " << guesses << "\n";
+		//asks player if they want to play again
+		cout << "Play again? (y/n): ";
+		cin >> playGame;
+	}
+	//if they input y, the while loop starts again
+	return 0;
+}
+
